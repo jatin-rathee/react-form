282bc6936503803bab65a569cd7a0de0916e6a01
jatin-rathee <jatinrathee2018@gmail.com>
2020-05-18 21:44:00 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const [email, setEmail] = useState('');
+	const [pass1, setPass1] = useState('');
+	const [dob, setDob] = useState();
 
-export default App;
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		console.log({
+			email,
+			password: pass1,
+			dob,
+		});
+	};
+
+	return (
+		<div className='App'>
+			<div className='form-container'>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Parent Email
+						<input
+							type='email'
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+							required
+						/>
+					</label>
+
+					<label>
+						Password
+						<input
+							type='password'
+							value={pass1}
+							onChange={e => setPass1(e.target.value)}
+							required
+						/>
+					</label>
+					<label>
+						Parent's Year of Birth
+						<input
+							type='text'
+							value={dob}
+							onChange={e => setDob(e.target.value)}
+							required
+						/>
+					</label>
+					<button id='submit'>Submit</button>
+				</form>
+			</div>
+		</div>
+	);
+}
